682bc951fbadd6346dfa611cab0d549f8800a430
simsevenx <cooperation@qq.com>
2017-12-29 14:53:45 +0800

    tokenRo null->0

--- x7-core/src/main/java/x7/core/web/TokenedRo.java
+++ x7-core/src/main/java/x7/core/web/TokenedRo.java
@@ -1,11 +1,15 @@
 package x7.core.web;
 
+import x7.core.util.StringUtil;
+
 public class TokenedRo implements Tokened{
 
 	private String passportId;//groupId
-	private String token;//登录后的我们自己的token
+	private String token;//
 	private String device;// WX | ANDROID | IOS | PC
 	public long getPassportId() {
+		if (StringUtil.isNullOrEmpty(passportId))
+			return 0;
 		return Long.valueOf(passportId);
 	}
 	public void setPassportId(String passportId) {
