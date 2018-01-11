c1fe57de6ffca7b6a214b3798386066c2f014600
simsevenx <cooperation@qq.com>
2018-01-11 15:49:25 +0800

    v1.0.0.00000000005

--- x7-core/src/main/java/x7/core/async/AsyncService.java
+++ x7-core/src/main/java/x7/core/async/AsyncService.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package x7.core.async;
 
 import java.util.concurrent.ArrayBlockingQueue;
@@ -6,8 +22,8 @@ import java.util.concurrent.BlockingQueue;
 import x7.core.async.IAsyncTask;
 
 /**
- * 重要的服务继承此类，以实现异步处理，每个子类将拥有独立的线程
- * @author wyan
+ * 
+ * @author Sim
  *
  */
 public class AsyncService extends Thread{
@@ -60,7 +76,7 @@ public class AsyncService extends Thread{
 	}
 	
 	/**
-	 * 终止服务, 如果需要消耗service对象, 请先也必须调用此方法
+	 * abort
 	 */
 	public void abort(){
 		this.accept(new IAsyncTask(){
