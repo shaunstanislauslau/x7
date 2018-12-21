7053a2685e5c1ab1b118be93821b3dac0ab85f20
simsevenx <cooperation@qq.com>
2018-12-21 11:56:38 +0800

    .R

--- x7-repository/src/main/java/x7/repository/DbType.java
+++ x7-repository/src/main/java/x7/repository/DbType.java
@@ -21,7 +21,5 @@ public class DbType {
 
     public static final String MYSQL = "mysql";
     public static final String ORACLE = "oracle";
-    public static final String DB2 = "db2";
-    public static final String SQLSERVER = "sqlserver";
     public static String value = "mysql";
 }
